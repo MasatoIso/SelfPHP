3daf693e8b71eccc5aee8bde4f7cd6cd07a23891
MasatoIso <04masato05@gmail.com>
2019-11-05 12:17:03 +0900

    Complete Chapter2

---
+++ chapter2/dimension3.php
@@ -0,0 +1,21 @@
+<?php
+$data = [
+    [
+        'name' => 'yamada',
+        'age' => '110',
+        'sex' => 'male',
+    ],
+    [
+        'name' => 'tanaka',
+        'age' => '1',
+        'sex' => 'female',
+    ],
+    [
+        'name' => 'sato',
+        'age' => '22',
+        'sex' => '?',
+    ],
+];
+
+print $data[2]['sex'];
+?>
