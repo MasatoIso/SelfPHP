721381233d10d459997edb83eef9b9278cf07d88
MasatoIso <04masato05@gmail.com>
2019-11-12 10:22:07 +0900

    Complete Chapter3

---
+++ chapter3/error.php
@@ -0,0 +1,10 @@
+<?php
+print 1/0;
+echo '<br />';
+@print 1/0;
+echo '<br />';
+
+echo $abc . $str;
+print 'error'
+//なぜかエラー制御演算子を使わなくてもエラーが表示されない
+?>
---
+++ chapter3/execute.php
@@ -0,0 +1,4 @@
+<?php
+$result = `pwd`;
+print $result;
+?>
